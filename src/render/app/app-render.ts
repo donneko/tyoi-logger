c844a3faca0e7d8ba0253279e9ed1bed572b3571
donneko <117331095+donneko@users.noreply.github.com>
2026-07-20 16:02:32 +0900

    add #6 レンダー追加

---
+++ src/render/app/app-render.ts
@@ -0,0 +1,30 @@
+import readline from "readline";
+
+export class Render {
+    private beforeIndex: number = 0;
+    private printData: string[] = [];
+
+    print(): void {
+        this.clear();
+
+        for (const message of this.printData) {
+            process.stdout.write(`${message}\n`);
+        }
+
+        this.beforeIndex = this.printData.length;
+    }
+    clear(): void {
+        // index の行へ移動
+        for (let i = 0; i < this.beforeIndex; i++) {
+            readline.moveCursor(process.stdout, 0, -1);
+            readline.cursorTo(process.stdout, 0);
+            readline.clearLine(process.stdout, 0);
+        }
+
+        this.beforeIndex = 0;
+    }
+
+    set(printData: string): void {
+        this.printData = printData.split("\n");
+    }
+}
