034f9b8cfb173dee9a862acab78e84ea0b11198c
donneko <117331095+donneko@users.noreply.github.com>
2026-07-22 15:25:52 +0900

    test(logger): index に型を修正

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,7 +1,3 @@
 export { Logger } from "./app/app-logger.js";
-export type {
-    LoggerCreateData,
-    LogType,
-    LoggerDependencies,
-    LogConfig,
-} from "./types/logger.type.js";
+export type * from "./types/logger.type.js";
+export type * from "./types/logger-dependencies.type.js";
