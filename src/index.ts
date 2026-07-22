3e188a7bf8d9368729a4d27c2b44c06a0e6f6c36
donneko <117331095+donneko@users.noreply.github.com>
2026-07-22 15:57:24 +0900

    refactor(index): 公開パスの修正

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./logger/index.js";
 export type * from "./logger/index.js";
 export { Ask } from "./ask/app/app-ask.js";
-export { Renderer } from "./render/app/app-render.js";
+export * from "./render/index.js";
